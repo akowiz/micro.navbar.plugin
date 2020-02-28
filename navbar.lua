b2f5d7dcaf00927aa4014729d925b5a5f6caf28a
Sylvain Pouilly <akorune@gmail.com>
2020-02-29 07:51:26 +0800

    The toggle message is now using proper syntax.

--- navbar.lua
+++ navbar.lua
@@ -8,8 +8,10 @@ function init()
     config.MakeCommand("navbar", navbarToggle, config.NoComplete)
     config.AddRuntimeFile("navbar", config.RTHelp, "help/navbar.md")
     config.TryBindKey("F5", "lua:navbar.navbarToggle", false)
+    visible = false
 end
 
 function navbarToggle(bp)
-	micro.InfoBar():Message("navbar toggle")
+    visible = not visible
+	micro.InfoBar():Message("navbar toggle "..tostring(visible))
 end
