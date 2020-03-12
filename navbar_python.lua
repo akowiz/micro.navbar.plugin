fdac0cde7bedc8fed9f2d5936765f72959e59fed
Sylvain Pouilly <akorune@gmail.com>
2020-03-12 21:14:14 +0800

    Implemented the Node object so that all our tests passes.

--- navbar_python.lua
+++ navbar_python.lua
@@ -1,9 +1,14 @@
 #!/bin/env lua
 
-local CLASSES = 1
-local FUNCTIONS = 2
-local CONSTANTS = 4
+T_NONE = 99
+T_CLASS = 1
+T_FUNCTION = 2
+T_CONSTANT = 4
 
+
+-------------------------------------------------------------------------------
+-- Helper Functions
+-------------------------------------------------------------------------------
 
 -- Split function with a python semantic
 --   see http://lua-users.org/wiki/SplitJoin
@@ -33,6 +38,26 @@ function string:split(sSeparator, nMax, bRegexp)
 end
 
 
+-------------------------------------------------------------------------------
+-- Data Structures
+-------------------------------------------------------------------------------
+
+Node = { name = '', kind = T_NONE, line = 0, indent = 0, parent = nil, }
+
+function Node:new(o, n, k, l, i, p)
+    o = o or {}
+    setmetatable(o, self)
+    self.__index = self
+
+    self.name = n or ''
+    self.kind = k or T_NONE
+    self.line = l or 0
+    self.indent = i or 0
+    self.parent = p
+    return o
+end
+
+
 -- Export the python structure of a buffer containing python code
 function string:export_structure_python()
     local aTable = {}
@@ -46,10 +71,15 @@ function string:export_structure_python()
 end
 
 
+-------------------------------------------------------------------------------
+-- Module
+-------------------------------------------------------------------------------
+
 return {
-  CLASSES = CLASSES,
-  FUNCTIONS = FUNCTIONS,
-  CONSTANTS = CONSTANTS,
-  split = split,
-  export_structure_python = export_structure_python,
+    T_CLASS = T_CLASS,
+    T_FUNCTION = T_FUNCTION,
+    T_CONSTANT = T_CONSTANT,
+    split = split,
+    export_structure_python = export_structure_python,
+    Node = Node,
 }
