b58acda3c6e80eb5fce63c1cc70ed24bf999d847
Sylvain Pouilly <akorune@gmail.com>
2020-03-12 16:09:59 +0800

    Imported a split function with python semantic from lua-users.org. All test passes.

--- navbar_python.lua
+++ navbar_python.lua
@@ -1,2 +1,43 @@
 #!/bin/env lua
 
+local CLASSES = 1
+local FUNCTIONS = 2
+local CONSTANTS = 4
+
+
+-- Split function with a python semantic
+--   see http://lua-users.org/wiki/SplitJoin
+function string:split(sSeparator, nMax, bRegexp)
+   assert(sSeparator ~= '')
+   assert(nMax == nil or nMax >= 1)
+
+   local aRecord = {}
+
+   if self:len() > 0 then
+      local bPlain = not bRegexp
+      nMax = nMax or -1
+
+      local nField, nStart = 1, 1
+      local nFirst,nLast = self:find(sSeparator, nStart, bPlain)
+      while nFirst and nMax ~= 0 do
+         aRecord[nField] = self:sub(nStart, nFirst-1)
+         nField = nField+1
+         nStart = nLast+1
+         nFirst,nLast = self:find(sSeparator, nStart, bPlain)
+         nMax = nMax-1
+      end
+      aRecord[nField] = self:sub(nStart)
+   end
+
+   return aRecord
+end
+
+
+
+
+return {
+  CLASSES = CLASSES,
+  FUNCTIONS = FUNCTIONS,
+  CONSTANTS = CONSTANTS,
+  split = split,
+}
