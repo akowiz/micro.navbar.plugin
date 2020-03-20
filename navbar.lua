921aecd1f42a4e860b7a7bd41fd0ab15bc332415
Sylvain Pouilly <akorune@gmail.com>
2020-03-20 17:40:50 +0800

    Implemented several features in our plugin.
    - display_content() now properly display a text tree extracted from the buffer.
    - micro.buf:FileType() fails quite often, we check the buffer name when FileType() returns 'unknown'.
    - added functions (from filemanager) to handle selecting lines, moving cursor, etc.
    - added command to jump_line in the main buffer depending on where we are in the tree_view.
    - added command to fold/unfold a node (not working at the moment).
    - assigned the key bindings for the 3 main commands (nvb_goto, nvb_close, nvb_open).

--- navbar.lua
+++ navbar.lua
@@ -13,14 +13,22 @@ local lgp = require('lang_python')
 local DISPLAY_NAME = 'navbar'
 
 -- Holds the micro.CurPane() we're manipulating
-local tree_view = nil
+local main_view = nil -- The original panel
+local tree_view = nil -- The navbar panel
+local node_list = nil
+
+-- Holds the views for the multiple panes we are manipulating
+local tree_views = {}
+
+
+
 
 
 -- Clear out all stuff in Micro's messenger
 local function clear_messenger()
-	micro.InfoBar():Reset()
+    micro.InfoBar():Reset()
     -- messenger:Reset()
-	-- messenger:Clear()
+    -- messenger:Clear()
 end
 
 local function display_content(buf)
@@ -34,24 +42,33 @@ local function display_content(buf)
     local bytes = util.String(buf:Bytes())
     local struc = lgp.export_structure_python(bytes)
     local ttree = lgp.tree_to_navbar(struc)
+
+    local display_text = {}
+
+    node_list = {}
     for k, v in ipairs(ttree) do
-        ret[#ret+1] = v:tree(ttype, 0) .. '\n\n'
+        display_text[#display_text+1] = v['text']
+        if v['node'] ~= nil then
+            node_list[#node_list+1] = v['node']
+        else
+            node_list[#node_list+1] = false
+        end
     end
 
-    return table.concat(ret)
+    return table.concat(display_text, '\n')
 end
 
 local function refresh_view(buf)
     clear_messenger()
 
     -- Delete everything
-	tree_view.Buf.EventHandler:Remove(tree_view.Buf:Start(), tree_view.Buf:End())
+    tree_view.Buf.EventHandler:Remove(tree_view.Buf:Start(), tree_view.Buf:End())
 
-	local ft = buf:FileType()
-	local fn = buf:GetName()
-	local content = ''
+    local ft = buf:FileType()
+    local fn = buf:GetName()
+    local content = ''
 
-	tree_view.Buf.EventHandler:Insert(buffer.Loc(0, 0), 'Symbols\n\n')
+    tree_view.Buf.EventHandler:Insert(buffer.Loc(0, 0), 'Symbols\n\n')
 
     -- There seems to be a bug in micro FileType automatic recognition.
     if     (ft == 'python') or ((ft == '') and (fn:ends_with('.py'))) then
@@ -59,37 +76,210 @@ local function refresh_view(buf)
         tree_view.Buf.EventHandler:Insert(buffer.Loc(0, 2), content)
 
     elseif (ft == 'lua') or ((ft == '') and (fn:ends_with('.lua'))) then
-		micro.InfoBar():Message(DISPLAY_NAME .. ": Support for lua files comming soon.")
+        micro.InfoBar():Message(DISPLAY_NAME .. ": Support for lua files comming soon.")
         tree_view.Buf.EventHandler:Insert(buffer.Loc(0, 2), content)
 
     else
-		micro.InfoBar():Error(DISPLAY_NAME .. ": Only python language is currently supported.")
+        micro.InfoBar():Error(DISPLAY_NAME .. ": Only python language is currently supported.")
         tree_view.Buf.EventHandler:Insert(buffer.Loc(0, 2), content)
     end
 
     tree_view:Tab():Resize()
 end
 
+-- Hightlights the line when you move the cursor up/down
+local function select_line(pane, last_y)
+    pane = pane or tree_view
+
+    if pane == tree_view then
+        -- Make last_y optional
+        if last_y ~= nil then
+            -- Don't let them move past the 2 first lines
+            if last_y > 1 then
+                -- If the last position was valid, move back to it
+                pane.Cursor.Loc.Y = last_y
+            end
+        elseif pane.Cursor.Loc.Y < 2 then
+            -- Put the cursor on the 1st item
+            pane.Cursor.Loc.Y = 2
+        end
+    else
+        last_y = last_y or 0
+    end
+
+    -- Puts the cursor back in bounds (if it isn't) for safety
+    pane.Cursor:Relocate()
+
+    -- Makes sure the cursor is visible (if it isn't)
+    -- (false) means no callback
+    pane:Center()
+
+    -- Highlight the current line where the cursor is
+    pane.Cursor:SelectLine()
+end
+
+
+-- Moves the cursor to the ".." in tree_view
+local function move_cursor_top(pane)
+    pane = pane or tree_view
+
+    -- line to go to
+    if pane == tree_view then
+        pane.Cursor.Loc.Y = 2
+    else
+        pane.Cursor.Loc.Y = 0
+    end
+
+    -- select the line after moving
+    select_line(pane)
+end
+
+-- Move the cursor to the top, but don't allow the action
+local function aftermove_if_tree(view)
+    if view == tree_view then
+        if view.Cursor.Loc.Y < 2 then
+            -- If it went past the "..", move back onto it
+            view.Cursor:DownN(2 - view.Cursor.Loc.Y)
+        end
+        select_line(view)
+    end
+end
+
+-- Used to fail certain actions that we shouldn't allow on the tree_view
+local function false_if_tree(view)
+    if view == tree_view then
+        return false
+    end
+end
+
+-- Select the line at the cursor
+local function selectline_if_tree(view)
+    if view == tree_view then
+        select_line(view)
+    end
+end
+
+local function clearselection_if_tree(view)
+    if view == tree_view then
+        -- Clear the selection when doing a find, so it doesn't copy the current line
+        view.Cursor:ResetSelection()
+    end
+end
+
+function onCursorUp(view)
+    selectline_if_tree(view)
+end
+
+function onCursorDown(view)
+    selectline_if_tree(view)
+end
+
+-- PageUp
+function onCursorPageUp(view)
+    aftermove_if_tree(view)
+end
+
+-- Ctrl-Up
+function onCursorStart(view)
+    aftermove_if_tree(view)
+end
+
+-- PageDown
+function onCursorPageDown(view)
+    selectline_if_tree(view)
+end
+
+-- Ctrl-Down
+function onCursorEnd(view)
+    selectline_if_tree(view)
+end
+
+function preFind(view)
+    -- Since something is always selected, clear before a find
+    -- Prevents copying the selection into the find input
+    clearselection_if_tree(view)
+end
+
+-- FIXME: doesn't work for whatever reason
+function onFind(view)
+    -- Select the whole line after a find, instead of just the input txt
+    selectline_if_tree(view)
+end
+
+function nvb_goto_line()
+    if tree_view ~= nil and (micro.CurPane() == tree_view) then
+        local tree_line
+        local node
+        -- Retrieve the line number to jump to
+        tree_line = tree_view.Cursor.Loc.Y - 1
+        node = node_list[tree_line]
+
+        if node ~= false and node.line ~= -1 then
+            main_view.Cursor.Loc.Y = node.line - 1
+            main_view.Cursor:Relocate()
+            main_view:Center()
+            main_view.Cursor:SelectLine()
+        end
+    end
+end
+
+function nvb_node_open()
+    -- FIXME: When the view is refresh, this will be lost!
+    -- We need to remember the closed status independantly from the building
+    -- of the list, and apply it after the list has been rebuilt.
+    -- We also might want a function to refresh the list without rebuilding
+    -- it fully.
+    -- We don't want to loose the closed status whenever we add/remove items
+    -- in our source file.
+    -- we might also want to have persistant saving between sessions.
+    if tree_view ~= nil and (micro.CurPane() == tree_view) then
+        local tree_line
+        local node
+        tree_line = tree_view.Cursor.Loc.Y - 1
+        node = node_list[tree_line]
+
+        if node ~= false and node:is_closed() then
+            node.closed = true
+            refresh_view()
+        end
+    end
+end
+
+function nvb_node_close()
+    -- FIXME: When the view is refresh, this will be lost!
+    if tree_view ~= nil and (micro.CurPane() == tree_view) then
+        local tree_line
+        local node
+        tree_line = tree_view.Cursor.Loc.Y - 1
+        node = node_list[tree_line]
+
+        if node ~= false and not node:is_closed() then
+            node.closed = false
+            refresh_view()
+        end
+    end
+end
+
 -- open_tree setup's the view
 local function open_tree()
-    -- Retrieve the current buffer
-    local buf = micro.CurPane().Buf
+    -- Save the current panel so that we can use it later
+    main_view = micro.CurPane()
 
-	-- Open a new Vsplit (on the very left)
-	micro.CurPane():VSplitIndex(buffer.NewBuffer("", DISPLAY_NAME), false)
-	-- Save the new view so we can access it later
-	tree_view = micro.CurPane()
+    -- Open a new Vsplit (on the very left)
+    micro.CurPane():VSplitIndex(buffer.NewBuffer("", DISPLAY_NAME), false)
+    -- Save the new view so we can access it later
+    tree_view = micro.CurPane()
 
-	-- Set the width of tree_view to 30% & lock it
+    -- Set the width of tree_view to 30% & lock it
     tree_view:ResizePane(30)
-	-- Set the type to unsavable
+    -- Set the type to unsavable
     -- tree_view.Buf.Type = buffer.BTLog
     tree_view.Buf.Type.Scratch = true
     tree_view.Buf.Type.Readonly = true
 
-	-- Set the various display settings, but only on our view (by using SetLocalOption instead of SetOption)
-	-- NOTE: Micro requires the true/false to be a string
-	-- Softwrap long strings (the file/dir paths)
+    -- Set the various display settings, but only on our view (by using SetLocalOption instead of SetOption)
+    -- NOTE: Micro requires the true/false to be a string
+    -- Softwrap long strings (the file/dir paths)
     tree_view.Buf:SetOptionNative("softwrap", true)
     -- No line numbering
     tree_view.Buf:SetOptionNative("ruler", false)
@@ -101,25 +291,28 @@ local function open_tree()
     tree_view.Buf:SetOptionNative("scrollbar", false)
 
     -- Display the content
-    refresh_view(buf)
+    refresh_view(main_view.Buf)
+
+    -- Move the cursor
+    move_cursor_top(tree_view)
 end
 
 -- close_tree will close the tree plugin view and release memory.
 local function close_tree()
-	if tree_view ~= nil then
-		tree_view:Quit()
-		tree_view = nil
-		clear_messenger()
-	end
+    if tree_view ~= nil then
+        tree_view:Quit()
+        tree_view = nil
+        clear_messenger()
+    end
 end
 
 -- toggle_tree will toggle the tree view visible (create) and hide (delete).
 function toggle_tree()
-	if tree_view == nil then
-		open_tree()
-	else
-		close_tree()
-	end
+    if tree_view == nil then
+        open_tree()
+    else
+        close_tree()
+    end
 end
 
 --- Initialize the navbar plugin.
@@ -127,6 +320,9 @@ function init()
     config.AddRuntimeFile("navbar", config.RTHelp, "help/navbar.md")
     config.TryBindKey("F5", "lua:navbar.toggle_tree", false)
     config.TryBindKey("Alt-n", "lua:navbar.toggle_tree", false)
+    config.TryBindKey("F6", "lua:navbar.nvb_goto_line", false)
+    config.TryBindKey("F7", "lua:navbar.nvb_node_open", false)
+    config.TryBindKey("F8", "lua:navbar.nvb_node_close", false)
 
     -- Lets the user have the filetree auto-open any time Micro is opened
     -- false by default, as it's a rather noticable user-facing change
@@ -135,7 +331,12 @@ function init()
 
     -- Open/close the tree view
     config.MakeCommand("navbar", toggle_tree, config.NoComplete)
-    -- Rename the file/dir under the cursor
+    -- Goto corresponding line
+    config.MakeCommand("nvb_goto", nvb_goto_line, config.NoComplete)
+    -- Close an open node
+    config.MakeCommand("nvb_close", nvb_node_close, config.NoComplete)
+    -- Open a closed node
+    config.MakeCommand("nvb_open", nvb_node_open, config.NoComplete)
 
     -- NOTE: This must be below the syntax load command or coloring won't work
     -- Just auto-open if the option is enabled
